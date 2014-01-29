1a7121ab7982b2ec1846fa60ced6b1e2f055a08a
Rocky He <rocky.jj.he@gmail.com>
2014-01-28 17:19:13 -0800

    Added initial GUI and server

---
+++ eece411/assign2/src/com/matei/eece411/A2/Server.java
@@ -0,0 +1,20 @@
+package com.matei.eece411.A2;
+
+import java.io.*;
+import java.rmi.*;
+
+import ChatImpl;
+
+public class Server
+{	
+	public static void main(String[] args)
+	{
+		try {
+			//Bind the name to new remote Chat object
+			Naming.rebind("Chat", new ChatImpl("Chat room"));
+			System.out.println("Server is ready");			
+		} catch (Exception e) {
+			System.out.println("Server failed: " + e);
+		}
+	}
+}
