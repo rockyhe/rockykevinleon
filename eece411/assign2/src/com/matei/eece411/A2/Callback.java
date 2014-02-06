9518bc4a5811ab7901a2c383573dc0d5e8f010da
gungun1010 <gungun1010@dhcp-128-189-244-46.ubcsecure.wireless.ubc.ca>
2014-02-06 02:58:23 -0800

    delete old folder

--- eece411/assign2/src/com/matei/eece411/A2/Callback.java
+++
@@ -1,8 +0,0 @@
-package com.matei.eece411.A2;
-import java.rmi.*;                                     
-
-public interface Callback extends Remote
-{
-    public void receive(String txt) throws RemoteException;
-}
-
