0c52ef31e531da5004f7020935314942c21f08eb
gungun1010 <gungun1010@dhcp-128-189-246-120.ubcsecure.wireless.ubc.ca>
2014-03-19 22:24:48 -0700

    Merge branch 'master' of https://github.com/rockyhe/rockykevinleon
    Conflicts:
    eece411/phase2/src/phase2Pack/Server.java

--- eece411/phase2/src/phase2Pack/Server.java
+++ eece411/phase2/src/phase2Pack/Server.java
@@ -14,8 +14,13 @@ public class Server {
 	//Constants
 	private static final int PORT = 5000;
 	private static final int MAX_NUM_CLIENTS = 50;
+	private static final int MAX_GOSSIP_MEMBERS = 4;
 	private static final int BACKLOG_SIZE = 50;
 	private static final String NODE_LIST_FILE = "nodeList.txt";
+	
+	//Make sure this value is larger than number of physical nodes
+	//Since potential max nodes is 100, then use 100 * 100 = 10000
+	private static final int NUM_PARTITIONS = 10000;
 
 	//Private members
 	private static ServerSocket servSock;
@@ -25,11 +30,34 @@ public class Server {
 	private static AtomicInteger shutdownFlag;
 	private static ExecutorService threadPool;
 
+	private static List<KVStore.Node> onlineNodeList;
+	//Sorted map for mapping hashed values to physical nodes
 	private static ConcurrentSkipListMap<String, KVStore.Node> nodes;
 
 	public static void main(String[] args)
 	{
 		try {
+			//Load the list of nodes
+			try {
+				Scanner s = new Scanner(new File(NODE_LIST_FILE));
+				onlineNodeList = new ArrayList<KVStore.Node>();
+				while (s.hasNext())
+				{
+					String node = s.next();
+					onlineNodeList.add(new KVStore.Node(new InetSocketAddress(node, PORT), true));
+				}
+				s.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("Cannot find node list file!");
+			} catch (Exception e) {
+				System.out.println("Error loading node list!");
+			}
+			
+			//Map the nodes to partitions
+			constructNodeMap();
+
+			displayNodeMap();
+
 			//Initialize members
 			servSock = new ServerSocket(PORT);
 			store = new ConcurrentHashMap<String,byte[]>();
@@ -39,30 +67,14 @@ public class Server {
 
 			//Create a fixed thread pool since we'll have at most MAX_NUM_CLIENTS concurrent threads
 			threadPool = Executors.newFixedThreadPool(MAX_NUM_CLIENTS);
-
-			try {
-				Scanner s = new Scanner(new File(NODE_LIST_FILE));
-				nodes = new ConcurrentSkipListMap<String, KVStore.Node>();
-				while (s.hasNext())
-				{
-					String node = s.next();
-					nodes.put(KVStore.getHash(node), new KVStore.Node(new InetSocketAddress(node, PORT), true));
-				}
-				s.close();
-			} catch (FileNotFoundException e) {
-				System.out.println("Cannot find node list file!");
-			} catch (Exception e) {
-				System.out.println("Error loading node list!");
-			}
-
-			DisplayNodeList();
-
 			System.out.println("Server is ready...");
 
 			//Create a new Producer thread for accepting client connections and adding them to the queue
 			Thread producer = new Thread(new Producer());
 			producer.start();
 
+			Thread gossiper = new Thread(new Gossiper());
+			gossiper.start();
 			//Create a new Consumer thread for servicing clients in the queue
 			Thread consumer = new Thread(new Consumer());
 			consumer.start();
@@ -70,8 +82,23 @@ public class Server {
 			System.out.println("Internal Server Error!");
 		}
 	}
+	
+	private static void constructNodeMap()
+	{
+		nodes = new ConcurrentSkipListMap<String, KVStore.Node>();
+		//Divide the hash space into NUM_PARTITIONS partitions
+		//with each physical node responsible for (NUM_PARTITIONS / number of nodes) hash ranges
+		int partitionsPerNode = NUM_PARTITIONS / onlineNodeList.size();
+		for (KVStore.Node node : onlineNodeList)
+		{
+			for (int i=0; i < partitionsPerNode; ++i)
+			{				
+				nodes.put(KVStore.getHash(node.address.getHostName() + i), node);
+			}
+		}
+	}
 
-	private static void DisplayNodeList()
+	private static void displayNodeMap()
 	{
 		for (Map.Entry<String, KVStore.Node> entry : nodes.entrySet())
 		{
@@ -81,6 +108,26 @@ public class Server {
 		}
 	}
 
+	private static class GossipClk extends TimerTask {
+	    public void run() {
+	       System.out.println("Hello World!"); 
+	    }
+	}
+	
+	private static class Gossiper implements Runnable {
+            public void run() {
+                Socket socket = null;
+                try{
+                    Random randomGenerator = new Random();
+                    int randomInt = randomGenerator.nextInt(MAX_GOSSIP_MEMBERS);
+                    System.out.println("random test: ");
+                    //socket = new Socket(server, servPort);
+                } catch (Exception e) {
+                    System.out.println("Internal Server Error!");
+                }
+            }             
+
+	}
 	private static class Producer implements Runnable
 	{
 		public void run()
@@ -134,7 +181,7 @@ public class Server {
 							//Create a new thread for each client connection
 							threadPool.execute(connection);
 							//System.out.println("New client executing.");
-						}
+						}		
 					}
 					else
 					{
@@ -142,6 +189,7 @@ public class Server {
 						System.out.println("closing down server!");
 						System.exit(0);
 					}
+					
 
 					//					if (concurrentClientCount.get() > 0)
 					//					{
