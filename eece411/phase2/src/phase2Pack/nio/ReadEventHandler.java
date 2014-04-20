3551eb70837c7af7726bb175324a955f7b748c2a
gungun1010 <gungun1010@dhcp-128-189-243-76.ubcsecure.wireless.ubc.ca>
2014-04-20 16:01:31 -0700

    fix readEvent handler

--- eece411/phase2/src/phase2Pack/nio/ReadEventHandler.java
+++ eece411/phase2/src/phase2Pack/nio/ReadEventHandler.java
@@ -15,8 +15,6 @@ public class ReadEventHandler implements EventHandler
     private static final int BUFFER_SIZE = 33;
 
     private Selector demultiplexer;
-    private ByteBuffer inputBuffer = ByteBuffer.allocate(BUFFER_SIZE);
-
     public ReadEventHandler(Selector demultiplexer)
     {
         this.demultiplexer = demultiplexer;
@@ -27,16 +25,18 @@ public class ReadEventHandler implements EventHandler
     {
         SocketChannel socketChannel = (SocketChannel) handle.channel();
 
+        //prepare memory location for buffer
+        ByteBuffer inputBuffer = ByteBuffer.allocate(BUFFER_SIZE);
         socketChannel.read(inputBuffer); // Read data from client
-
         inputBuffer.flip();
         // Rewind the buffer to start reading from the beginning
-
+        
         byte[] buffer = new byte[inputBuffer.limit()];
         inputBuffer.get(buffer);
         inputBuffer.flip();
         System.out.println("Received message from client : " + Arrays.toString(buffer));
-
+     
+        inputBuffer.clear();
         // Rewind the buffer to start reading from the beginning
         // Register the interest for writable readiness event for
         // this channel in order to echo back the message
