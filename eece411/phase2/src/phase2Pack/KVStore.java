23fe2835574ccf01c9906a73fbe46853cd700c38
Rocky He <rocky.jj.he@gmail.com>
2014-04-24 15:54:51 -0700

    Added comments to KVStore methods

--- eece411/phase2/src/phase2Pack/KVStore.java
+++ eece411/phase2/src/phase2Pack/KVStore.java
@@ -18,6 +18,11 @@ public class KVStore
         this.store = new ConcurrentHashMap<String, byte[]>();
     }
 
+    /**
+     * Puts the given value into the store, mapped to the given key.
+     * If there is already a value corresponding to the key, then the value is overwritten.
+     * If the number of key-value pairs is KVSTORE_SIZE, the store returns out of space error.
+     */
     public void put(String hashedKey, byte[] value) throws OutOfSpaceException
     {
         if (store.size() < KVSTORE_SIZE)
@@ -30,6 +35,10 @@ public class KVStore
         }
     }
 
+    /**
+     * Returns the value associated with the given key.
+     * If there is no such key in the store, the store returns key not found error.
+     */
     public byte[] get(String hashedKey) throws InexistentKeyException
     {
         // If key doesn't exist on this node's local store
@@ -40,6 +49,10 @@ public class KVStore
         return store.get(hashedKey);
     }
 
+    /**
+     * Removes the value associated with the given key.
+     * If there is no such key in the store, the store returns key not found error.
+     */
     public void remove(String hashedKey) throws InexistentKeyException
     {
         if (!store.containsKey(hashedKey))
